6e6d326216c55d7f5f3c22910b6395e4cc516459
Stephen Hunter <jagter@gmail.com>
2019-12-14 14:19:34 +0200

    update config

--- config.py
+++ config.py
@@ -3,14 +3,13 @@ import json
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    
+
     if os.path.exists("tmp/cloudbudget-secrets.json"):
         with open("tmp/cloudbudget-secrets.json") as f:
             secrets = json.load(f)
             SECRET_KEY = secrets['cloudbudget-secret-key']
             SQLALCHEMY_DATABASE_URI = secrets['connection']
     else:
-        with file.open("tmp/")
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'lame-secret-key'
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         f'sqlite:///{os.path.join(basedir,"app.db")}'
